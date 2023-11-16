af8164514e4da9baed53c926c2d8063f09219bf9
Ankit <ankittamori@gamil.com>
2023-11-17 01:50:05 +0530

    first program

---
+++ Day_1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+pet = input("What's your pet's name?\n")
+street = input("What's the name of the city you grew up in?\n")
+print("Your band name could be " + pet + " " + street)
